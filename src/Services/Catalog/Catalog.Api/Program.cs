b161af8364bc24212cf98e226b6fd055f71a2e9d
Chi Cuong <nguyenchicuongk21@gmail.com>
2024-06-29 22:36:11 +0700

    Fix: reinstall carter

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -2,6 +2,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
